3eb0bfdd9b1417e667eab18c90fe9fb587719c3b
karen matsunaga <Karen.f.matsunaga@gmail.com>
2025-07-16 20:00:18 -0300

    2025-07-16 Added custom Data Component

---
+++ src/main/java/net/karen/mccoursemod/component/ModDataComponentTypes.java
@@ -0,0 +1,31 @@
+package net.karen.mccoursemod.component;
+
+import com.mojang.serialization.Codec;
+import net.karen.mccoursemod.MccourseMod;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.component.DataComponentType;
+import net.minecraft.core.registries.Registries;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+import java.util.function.UnaryOperator;
+
+public class ModDataComponentTypes {
+    public static final DeferredRegister<DataComponentType<?>> DATA_COMPONENT_TYPES =
+            DeferredRegister.create(Registries.DATA_COMPONENT_TYPE, MccourseMod.MOD_ID);
+
+    // Coordinates custom tag -> X, Y, Z positions
+    public static final RegistryObject<DataComponentType<BlockPos>> COORDINATES = register("coordinates",
+            builder -> builder.persistent(BlockPos.CODEC));
+
+    // Multiplier custom tag
+    public static final RegistryObject<DataComponentType<Integer>> MULTIPLIER = register("multiplier",
+            integerBuilder -> integerBuilder.persistent(Codec.INT));
+
+    private static <T>RegistryObject<DataComponentType<T>> register(String name,
+                                                                    UnaryOperator<DataComponentType.Builder<T>> builderOperator) {
+        return DATA_COMPONENT_TYPES.register(name, () -> builderOperator.apply(DataComponentType.builder()).build());
+    }
+
+    public static void register(IEventBus eventBus) { DATA_COMPONENT_TYPES.register(eventBus); }
+}
