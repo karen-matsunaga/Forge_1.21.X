f058cfcf6ebd52fa2f86ec38e5f169d492917dff
karen matsunaga <Karen.f.matsunaga@gmail.com>
2025-07-17 12:20:26 -0300

    2025-07-17 Fixed Alexandrite button, door, fence, fence gate, pressure plate, slab, stairs, trapdoor and wall

--- src/main/java/net/karen/mccoursemod/datagen/ModBlockStateProvider.java
+++ src/main/java/net/karen/mccoursemod/datagen/ModBlockStateProvider.java
@@ -17,16 +17,52 @@ public class ModBlockStateProvider extends BlockStateProvider {
 
     @Override
     protected void registerStatesAndModels() {
+        // Custom block
         blockWithItem(ModBlocks.ALEXANDRITE_BLOCK);
         blockWithItem(ModBlocks.RAW_ALEXANDRITE_BLOCK);
         blockWithItem(ModBlocks.ALEXANDRITE_ORE);
         blockWithItem(ModBlocks.ALEXANDRITE_DEEPSLATE_ORE);
         blockWithItem(ModBlocks.MAGIC_BLOCK);
 
+        // Custom stairs
+        stairsBlock(ModBlocks.ALEXANDRITE_STAIRS.get(), blockTexture(ModBlocks.ALEXANDRITE_BLOCK.get()));
+
+        // Custom slab
+        slabBlock(ModBlocks.ALEXANDRITE_SLAB.get(), blockTexture(ModBlocks.ALEXANDRITE_BLOCK.get()),
+                                                    blockTexture(ModBlocks.ALEXANDRITE_BLOCK.get()));
+
+        // Custom button
+        buttonBlock(ModBlocks.ALEXANDRITE_BUTTON.get(), blockTexture(ModBlocks.ALEXANDRITE_BLOCK.get()));
+
+        // Custom pressure plate
+        pressurePlateBlock(ModBlocks.ALEXANDRITE_PRESSURE_PLATE.get(), blockTexture(ModBlocks.ALEXANDRITE_BLOCK.get()));
+
+        // Custom fence
+        fenceBlock(ModBlocks.ALEXANDRITE_FENCE.get(), blockTexture(ModBlocks.ALEXANDRITE_BLOCK.get()));
+
+        // Custom fence gate
+        fenceGateBlock(ModBlocks.ALEXANDRITE_FENCE_GATE.get(), blockTexture(ModBlocks.ALEXANDRITE_BLOCK.get()));
+
+        // Custom wall
+        wallBlock(ModBlocks.ALEXANDRITE_WALL.get(), blockTexture(ModBlocks.ALEXANDRITE_BLOCK.get()));
+
+        // Custom door
+        doorBlockWithRenderType(ModBlocks.ALEXANDRITE_DOOR.get(), modLoc("block/alexandrite_door_bottom"),
+                                                                  modLoc("block/alexandrite_door_top"), "cutout");
+
+        // Custom trapdoor
+        trapdoorBlockWithRenderType(ModBlocks.ALEXANDRITE_TRAPDOOR.get(), modLoc("block/alexandrite_trapdoor"),
+                                                                 true, "cutout");
+
+        // Custom stairs
         blockItem(ModBlocks.ALEXANDRITE_STAIRS);
+        // Custom slab
         blockItem(ModBlocks.ALEXANDRITE_SLAB);
+        // Custom pressure plate
         blockItem(ModBlocks.ALEXANDRITE_PRESSURE_PLATE);
+        // Custom fence gate
         blockItem(ModBlocks.ALEXANDRITE_FENCE_GATE);
+        // Custom trapdoor
         blockItem(ModBlocks.ALEXANDRITE_TRAPDOOR, "_bottom");
     }
 
